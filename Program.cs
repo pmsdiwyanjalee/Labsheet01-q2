54a3780c4752df3e4141391f2cb03a8d2395aeeb
User <User@DESKTOP-2OOA2TK>
2024-01-24 16:20:07 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Enter 10 numbers:");
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"Enter number{i}:");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    string result = (number % 2 == 0) ? "even" : "odd";
+                    Console.WriteLine($"The number {number} is {result}.");
+                }
+        }
+    }
+
+}
